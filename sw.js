d9e149b8872b728e5612e0b9530980109b06a7ab
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-06-15 23:56:21 +0000

    backend picker label: bolder, larger, full contrast

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'last-d74d6e54';
+const CACHE_VERSION = 'last-1b457f04';
 
 const PRECACHE_URLS = [
   './',

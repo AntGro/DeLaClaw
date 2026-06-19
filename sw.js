8efd97345b1bf5bb80d9dc704b63f8d046119b3f
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-06-19 13:22:08 +0100

    feat: inline saving spinner on list quick-add during Drive flush
    Replaces the invisible header badge with feedback right where the
    user is looking: the '+' button becomes a spinner and the input
    is disabled while the write flushes to Google Drive. Restores
    on completion regardless of success/failure.
    
    Checked: versioning, i18n [~], docs [~], readme [~], checklist [~], tests [~], welcome [~], prompts [~], xss [~]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'dlc-1.141';
+const CACHE_VERSION = 'dlc-1.142';
 
 const PRECACHE_URLS = [
   './',

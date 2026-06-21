68224ec95870d3ff880eef7842608f5634c9b6a5
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-06-21 03:51:33 +0200

    fix: delete button color on colored headers for all pages
    todo-cat-delete-btn was only styled for .todo-cat-header context
    (TODOs/Habits). Added .project-card-header selectors so Flashcards,
    Vestiaire, and Lists get the same white-on-colored-header treatment.
    
    Checked: versioning [x], i18n [~], docs [~], readme [~], checklist [~], tests [~], welcome [~], prompts [~], xss [~]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'dlc-1.200';
+const CACHE_VERSION = 'dlc-1.201';
 
 const PRECACHE_URLS = [
   './',

fbbd27230e8ab4ea4d03bcb41cea55020b56c56b
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-06-21 23:55:32 +0000

    docs: add README homepage, auto-detect basePath for docs.delaclaw.com
    Docsify basePath switches between /docs-site/ (GitHub Pages path)
    and / (docs.delaclaw.com subdomain) based on hostname. Added
    README.md landing page for docs-site. Fixed sidebar Home link.
    
    Checked: versioning [x], i18n [~], docs [x], readme [~], checklist [~], tests [~], welcome [~], prompts [~], xss [~]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'dlc-1.208';
+const CACHE_VERSION = 'dlc-1.209';
 
 const PRECACHE_URLS = [
   './',

21c3f735cf2acd2756d9170ee7c7e9574200e644
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-06-27 23:26:09 +0000

    fix: footer docs link → https://docs.delaclaw.com/#/
    Updated the Docs link in the footer from relative `docs-site/`
    to the canonical domain `https://docs.delaclaw.com/#/`.
    
    Checked: versioning [~], i18n [~], docs [~], readme [~], checklist [~], tests [~], welcome [~], prompts [~], xss [~]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'dlc-1.236';
+const CACHE_VERSION = 'dlc-1.237';
 
 const PRECACHE_URLS = [
   './',

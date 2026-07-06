e2bb1865f06a7aada286fbe67ab3634dc5945800
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-07-06 23:49:21 +0100

    style: widen auth modal to 480px to avoid URL line-wrap
    Checked: versioning [x], i18n [~], docs [~], readme [~], checklist [~], tests [~], welcome [~], prompts [~], xss [~]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'dlc-1.317';
+const CACHE_VERSION = 'dlc-1.318';
 
 const PRECACHE_URLS = [
   './',

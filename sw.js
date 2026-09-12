a70f3b901df43683abae3cea7048517d9cbc0a70
Antoine Grosnit <antoine.grosnit70@gmail.com>
2026-09-12 15:44:36 +0100

    chore(codemap): refresh generated CODEMAP timestamps after docs commit
    Checked: versioning [x], i18n [~], docs [~], readme [~], checklist [~], tests [~], welcome [~], prompts [~], xss [~]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for DeLaClaw (PWA)
 // CACHE_VERSION is updated by the pre-commit hook from VERSION file
-const CACHE_VERSION = 'dlc-2.0.34';
+const CACHE_VERSION = 'dlc-2.0.35';
 
 
 const PRECACHE_URLS = [
